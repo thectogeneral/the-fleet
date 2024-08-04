bfb4e2df6c038c752f3e9160a596a5538aeb3a7c
thectogeneral <salemolorundare@gmail.com>
2024-08-04 17:59:51 +0100

    update workflow

--- config/config.js
+++ config/config.js
@@ -2,19 +2,19 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
+    username: process.env.LOCAL_DB_USERNAME,
+    password: process.env.LOCAL_DB_PASSWORD,
+    database: process.env.LOCAL_DB_NAME,
+    host: process.env.LOCAL_DB_HOST,
     dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
-  test: {
-    username: 'your_username',
-    password: 'your_password',
-    database: 'your_database',
-    host: 'localhost',
-    dialect: 'postgres',
-  },
+  
   production: {
     username: process.env.DB_USERNAME,
     password: process.env.DB_PASSWORD,
--- infra-setup/vm-setup/main.tf
+++ infra-setup/vm-setup/main.tf
@@ -9,6 +9,149 @@ terraform {
   required_version = ">= 1.2.0"
 }
 
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+# Provider configuration
 provider "aws" {
-  region  = "us-west-2"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1"
+}
+
+# Create a VPC
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+}
+
+# Create an Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
+}
+
+# Create Subnets in Multiple AZs
+data "aws_availability_zones" "available" {}
+
+resource "aws_subnet" "main" {
+  count             = 2
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + 1}.0/24"
+  availability_zone = element(data.aws_availability_zones.available.names, count.index)
+}
+
+# Create a Route Table
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
+  }
+}
+
+# Associate the Route Table with the Subnets
+resource "aws_route_table_association" "main" {
+  count          = 2
+  subnet_id      = aws_subnet.main[count.index].id
+  route_table_id = aws_route_table.main.id
+}
+
+# Create a Security Group
+resource "aws_security_group" "main" {
+  vpc_id = aws_vpc.main.id
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 433
+    to_port     = 433
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 5432
+    to_port     = 5432
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Create an EC2 Instance
+resource "aws_instance" "app_server" {
+  ami           = "ami-04a81a99f5ec58529"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main[0].id
+  vpc_security_group_ids     = [aws_security_group.main.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "AppServer"
+  }
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt-get update -y
+              sudo apt-get install -y postgresql-client
+              EOF
+}
+
+# Create a PostgreSQL RDS Instance
+resource "aws_db_instance" "postgres" {
+  allocated_storage    = 20
+  engine               = "postgres"
+  engine_version       = "16.3"
+  instance_class       = "db.t3.micro"
+  db_name                 = "mydatabase"
+  username             = "powerlabsadmin"
+  password             = "admin123"
+  parameter_group_name = "default.postgres16"
+  skip_final_snapshot  = true
+  publicly_accessible  = true
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  db_subnet_group_name = aws_db_subnet_group.main.name
+
+  tags = {
+    Name = "PostgreSQLDatabase"
+  }
+}
+
+# Create a DB Subnet Group
+resource "aws_db_subnet_group" "main" {
+  name       = "main-subnet-group"
+  subnet_ids = aws_subnet.main[*].id
+
+  tags = {
+    Name = "MainSubnetGroup"
+  }
 }
